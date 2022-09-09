3bcac050a9c602136160611c77ace46da89d366b
Albano Pereira <albano.pereira@b2wdigital.com>
2022-09-09 11:14:24 -0300

    backend done

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const allowCors = require('./cors')
 
 module.exports = () => {
     const app = express();
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: "true"}))
+    app.use(allowCors)
     consign().include("controllers").into(app)
     return app
 }
